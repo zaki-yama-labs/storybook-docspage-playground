6266c1b332095513e43945141b0127a9d5f6bce5
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-09-25 01:25:34 +0900

    Add `label` props

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button as LDSButton } from 'react-lightning-design-system';
 
-export const Button = ({ type }) => <LDSButton type={type}>Hello</LDSButton>
+export const Button = ({ type, label }) => <LDSButton type={type} label={label}></LDSButton>
 
 Button.propTypes = {
   type: PropTypes.oneOf(['neutral', 'brand', 'destructive']),
   label: PropTypes.string,
 };
 
+Button.defaultProps = {
+  label: 'Hello',
+}
